c7150bcc1660c93a6c123c66b2cc1ebe406cad21
Mgen <m@mgenware.com>
2018-02-19 23:42:04 +0800

    Initial commit

---
+++ src/entries/html-string/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default class HTMLStringApp extends React.Component {
+    render() {
+        return (React.createElement("h1", null, "haha"));
+    }
+}
---
+++ src/entries/html-string/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class HTMLStringApp extends React.Component {
+  render() {
+    return (
+      <h1>HTMLStringApp</h1>
+    );
+  }
+}
