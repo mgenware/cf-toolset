383f319c8ddf86dbf00adbb71387f8d96d82e812
Mgen <m@mgenware.com>
2018-02-28 22:58:37 +0800

    Migrate to typescript

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HTMLStringApp from './toolset/html-string/index';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+<BrowserRouter>
+  <div className="App">
+    <ul>
+      <li><Link to="/html-string">HTML String</Link></li>
+    </ul>
+    <hr />
+    <Route exact={true} path="/html-string" component={HTMLStringApp} />
+  </div>
+</BrowserRouter>
+    );
+  }
+}
+
+export default App;
--- src/entries/html-string/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export default class HTMLStringApp extends React.Component {
-    render() {
-        return (React.createElement("h1", null, "haha"));
-    }
-}
