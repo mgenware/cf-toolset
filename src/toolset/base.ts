ad4dbb8712b39bcce23f0aad4709e45966f5dfa6
Mgen <m@mgenware.com>
2018-03-02 01:27:55 +0800

    Set up basic styles

---
+++ src/toolset/base.ts
@@ -0,0 +1,3 @@
+import './_oth/base.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
--- src/toolset/html-string/main.tsx
+++ src/toolset/html-string/main.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
+import '../base';
 
 export default class HTMLStringApp extends React.Component {
   render() {
     return (
-      <h1>HTMLStringApp</h1>
+<div className="container">
+  <form>
+    <div className="form-group">
+      <label htmlFor="textInput">Input</label>
+      <textarea autoFocus={true} className="form-control cft-code-edit" id="textInput" rows={10} />
+    </div>
+  </form>
+</div>
     );
   }
 }
