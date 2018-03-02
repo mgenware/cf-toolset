00ac535eb1eb7f7de7d8b973a3f410688df3592d
Mgen <m@mgenware.com>
2018-03-02 12:07:47 +0800

    Add common controls

---
+++ src/toolset/_oth/controls.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import '../base';
+
+export interface CodeViewProps {
+  content?: string;
+}
+
+export class CodeView extends React.Component<CodeViewProps, {}> {
+  private textarea: HTMLTextAreaElement|null;
+
+  constructor(props: CodeViewProps) {
+    super(props);
+  }
+
+  render() {
+    const { props } = this;
+    return (
+<textarea
+  className="cft-code-view"
+  readOnly={true}
+  style={{ width: '100%' }}
+  rows={10}
+  value={props.content}
+  ref={(input) => this.textarea = input} 
+/>
+    );
+  }
+}
+
+export interface CodeEditorProps {
+  autoFocus?: boolean;
+  onChange: (content: string) => void;
+}
+
+export class CodeEditor extends React.Component<CodeEditorProps, {}> {
+  constructor(props: CodeEditorProps) {
+    super(props);
+  }
+
+  render() {
+    const { props } = this;
+    return (
+<textarea 
+  autoFocus={props.autoFocus}
+  className="form-control cft-code-edit"
+  rows={10}
+  onChange={(e) => props.onChange(e.target.value)}
+/>
+    );
+  }
+
+}
