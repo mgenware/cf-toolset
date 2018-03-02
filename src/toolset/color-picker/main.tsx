401311c91dddb609d873f6f6ad73192e9a35740f
Mgen <m@mgenware.com>
2018-03-03 05:22:53 +0800

    color-picker: Display both RGB and RGBA colors

--- src/toolset/color-picker/main.tsx
+++ src/toolset/color-picker/main.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import { ChromePicker, ColorResult, Color, PhotoshopPicker, HuePicker, AlphaPicker } from 'react-color';
+const rgbToHex = require('rgb-hex');
 
 interface State {
   color: ColorResult|string;
+
+  rgbHex?: string;
+  rgbaHex?: string;
+  rgbCss?: string;
+  rgbaCss?: string;
 }
 
 export default class ColorPicker extends React.Component<object, State> {
@@ -26,7 +32,23 @@ export default class ColorPicker extends React.Component<object, State> {
     return (
 <div className="container">
   <h2>RGB</h2>
-  <code>{state.color}</code>
+  <p>
+    <kbd>Hex</kbd>
+    <code className="ml-2">{state.rgbHex}</code>
+  </p>
+  <p>
+    <kbd>CSS</kbd>
+    <code className="ml-2">{state.rgbCss}</code>
+  </p>
+  <h2>RGBA</h2>
+  <p>
+    <kbd>Hex</kbd>
+    <code className="ml-2">{state.rgbaHex}</code>
+  </p>
+  <p>
+    <kbd>CSS</kbd>
+    <code className="ml-2">{state.rgbaCss}</code>
+  </p>
   <div className="row">
     <div className="col-sm-auto">
       <ChromePicker
@@ -60,7 +82,14 @@ export default class ColorPicker extends React.Component<object, State> {
     );
   }
 
-  handleChangeComplete = (color: ColorResult) => {
-    this.setState({ color: color });
+  private handleChangeComplete = (color: ColorResult) => {
+    const { rgb } = color;
+    this.setState({
+      color: color,
+      rgbHex: '#' + rgbToHex(rgb.r, rgb.g, rgb.b),
+      rgbaHex: '#' + rgbToHex(rgb.r, rgb.g, rgb.b, rgb.a),
+      rgbCss: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`,
+      rgbaCss: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b}, ${rgb.a})`,
+    });
   }
 }
