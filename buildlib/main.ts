ade038e71505d3df973d6fe88ba0e945bb8fe4fb
Mgen <m@mgenware.com>
2018-03-03 17:03:41 +0800

    Add a dummy entry.tsx.txt to make generated lib files work in cft

--- buildlib/main.ts
+++ buildlib/main.ts
@@ -2,7 +2,9 @@ import * as mfs from 'm-fs';
 import * as nodepath from 'path';
 import * as util from 'util';
 import { exec } from 'child_process';
+import { unlink } from 'fs';
 const execAsync = util.promisify(exec);
+const unlinkAsync = util.promisify(unlink);
 
 /* tslint:disable no-console */
 /* tslint:disable max-line-length */
@@ -14,11 +16,20 @@ const execAsync = util.promisify(exec);
   console.log('Building toolset...');
   console.log(entryNames);
 
+  console.log('Reading scripts...');
+  const rawEntryScript = await mfs.readTextFileAsync('./inject/entry.tsx.txt');
+
   const promises = entryNames.map(async (name) => {
+    // create entry file
+    const entryFile = `${toolsetPath}/${name}/entry.tsx`;
+    const entryContent = rawEntryScript.replace('__APP_ID__', `cft_${name}_app`);
+    await mfs.writeFileAsync(entryFile, entryContent);
+
     const distFile = nodepath.join(__dirname, `dist/${name}.prod.js`);
-    const cmd = `cd "${toolsetPath}" && rollup -c rollup.config.js -i "${name}/main.tsx" -o "${distFile}"`;
+    const cmd = `cd "${toolsetPath}" && rollup -c rollup.config.js -i "${name}/entry.tsx" -o "${distFile}"`;
     console.log(cmd);
-    return await execAsync(cmd);
+    await execAsync(cmd);
+    await unlinkAsync(entryFile);
   });
   await Promise.all(promises);
 })();
