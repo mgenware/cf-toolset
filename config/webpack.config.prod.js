86b6060ebf27051b20c99e5158aabdbb4f7fbccb
Mgen <m@mgenware.com>
2018-03-04 02:22:22 +0800

    Set react, react-dom and jquery as externals

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -77,6 +77,11 @@ module.exports = {
         .relative(paths.appSrc, info.absoluteResourcePath)
         .replace(/\\/g, '/'),
   },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    jquery: 'jQuery',
+  },
   resolve: {
     // This allows you to set a fallback for where Webpack should look for modules.
     // We placed these paths second because we want `node_modules` to "win"
