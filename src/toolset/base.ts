494d1fa322420a98772023570a0629c62b70528d
Mgen <m@mgenware.com>
2018-03-04 00:22:54 +0800

    Import all common views in base.ts

--- src/toolset/base.ts
+++ src/toolset/base.ts
@@ -2,4 +2,8 @@ import './_app/app.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// base controls
+import './_app/controls';
+import './_baseEncoderView/main';
+
 export { default as app } from './_app/app';
