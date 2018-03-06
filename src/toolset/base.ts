45986f8f4f799c62fc253ec9128836e7698ef9a0
Mgen <m@mgenware.com>
2018-03-06 19:13:41 +0800

    Fix errors in base.ts

--- src/toolset/base.ts
+++ src/toolset/base.ts
@@ -1,10 +1,10 @@
-import './_app/app.css';
+import './common/app.css';
 
 import 'react';
 import 'react-dom';
 
 // base controls
-import './_app/controls';
-import './_baseEncoderView/main';
+import './common/controls';
+import './common/baseEncoderView/main';
 
 export { default as app } from './common/app';
