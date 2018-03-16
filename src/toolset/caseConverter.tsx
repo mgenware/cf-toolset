3dab53b1241b1f63f45711905fd9737ebdc5e2fc
Mgen <m@mgenware.com>
2018-03-17 02:08:03 +0800

    case-converter: Add capitalized case and sentence case

--- src/toolset/caseConverter.tsx
+++ src/toolset/caseConverter.tsx
@@ -1,8 +1,9 @@
 import { BaseActionView } from 'common/baseActionView';
+import { Fx23StringReader } from 'fx23';
 import app from 'common/app';
 
 enum ActionType {
-  uppercase, lowercase, capitalized
+  uppercase, lowercase, capitalized, sentence,
 }
 
 export default class CaseConverter extends BaseActionView {
@@ -18,17 +19,55 @@ export default class CaseConverter extends BaseActionView {
         return src.toLowerCase();
       }
 
+      case ActionType.capitalized: {
+        return this.toCapitalized(src);
+      }
+
+      case ActionType.sentence: {
+        return this.toSentenceCase(src);
+      }
+
       default: return '';
     }
   }
 
   actionNames(): string[] {
     const { ls } = this;
-    return [ls.uppercase, ls.lowercase, ls.capitalized];
+    return [ls.uppercase, ls.lowercase, ls.capitalized, ls.sentence];
   }
 
   title(): string {
     return app.localizedMap(localizedStrings()).title;
+  }
+
+  private toCapitalized(src: string): string {
+    const reader = new Fx23StringReader(src);
+    let res = '';
+    while (reader.hasNext()) {
+      // ignore non-letters
+      res += reader.collectWhile((c) => !c.match(/[a-z]/i));
+      if (reader.hasNext()) {
+        res += reader.next().toUpperCase();
+        res += reader.collectWhile((c) => c.trim() !== '');
+      }
+    }
+    return res;
+  }
+
+  private toSentenceCase(src: string): string {
+    const reader = new Fx23StringReader(src);
+    let res = '';
+    while (reader.hasNext()) {
+      // ignore non-letters
+      res += reader.collectWhile((c) => !c.match(/[a-z]/i));
+      if (reader.hasNext()) {
+        res += reader.next().toUpperCase();
+        res += reader.collectWhile((c) => c.trim() !== '.');
+        // jump over the . if needed
+        res += reader.next();
+      }
+    }
+    return res;
   }
 }
 
@@ -38,12 +77,14 @@ function localizedStrings(): { [id1: string]: { [id2: string]: string; }; } {
       uppercase: 'UPPERCASE',
       lowercase: 'lowercase',
       capitalized: 'Capitalized Case',
+      sentence: 'Sentence Case',
     },
   
     cn: {
       uppercase: '大小',
       lowercase: '小写',
       capitalized: '首字母大写',
+      sentence: '每句首字母大写',
     }
   };
 }
