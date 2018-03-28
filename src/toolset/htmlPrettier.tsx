307dfaaefe8ea285fd4516fa9eb7237b73893b24
Mgen <m@mgenware.com>
2018-03-29 02:01:36 +0800

    htmlPrettier: Add indent option

--- src/toolset/htmlPrettier.tsx
+++ src/toolset/htmlPrettier.tsx
@@ -2,12 +2,38 @@ import * as React from 'react';
 import app from 'common/app';
 import CodeEditor from 'common/widgets/codeEditor';
 import CodeView from 'common/widgets/codeView';
-import * as prettier from 'js-beautify';
+import { html_beautify } from 'js-beautify';
+
+interface HTMLBeautifyOptions {
+  indent_size?: number;
+  indent_char?: string;
+  indent_with_tabs?: boolean;
+  indent_handlebars?: boolean;
+  eol?: string;
+  end_with_newline?: boolean;
+  preserve_newlines?: boolean;
+  max_preserve_newlines?: number;
+  indent_inner_html?: boolean;
+  brace_style?: 'collapse-preserve-inline'|'collapse'|'expand'|'end-expand'|'none';
+  indent_scripts?: 'keep'|'separate'|'normal';
+  wrap_line_length?: number;
+  wrap_attributes?: 'auto'|'force' ;
+  wrap_attributes_indent_size?: number;
+  unformatted?: string[];
+  content_unformatted?: string[];
+  extra_liners?: string|string[];
+}
+
+enum IndentOpt {
+  twoSpaces = '2s',
+  fourSpaces = '4s',
+  tab = 'tab',
+}
 
 interface State {
   src: string;
   dest: string;
-  indent: string;
+  indentOpt: IndentOpt;
 }
 
 export default class HTMLPrettier extends React.Component<object, State> {
@@ -21,7 +47,7 @@ export default class HTMLPrettier extends React.Component<object, State> {
     this.state = {
       src: '',
       dest: '',
-      indent: '4s',
+      indentOpt: IndentOpt.twoSpaces,
     };
   }
 
@@ -32,6 +58,16 @@ export default class HTMLPrettier extends React.Component<object, State> {
     return (
 <div className="container">
   <h2>{ls.title}</h2>
+  <form>
+    <div className="form-group">
+      <label htmlFor="selectIndent">{ls.indent}</label>
+      <select className="form-control" id="selectIndent" value={state.indentOpt} onChange={(e) => this.setState({ indentOpt: IndentOpt[e.target.value] })}>
+        <option value={IndentOpt.twoSpaces}>{'2 ' + ls.spaces}</option>
+        <option value={IndentOpt.fourSpaces}>{'4 ' + ls.spaces}</option>
+        <option value={IndentOpt.tab}>{ls.tab}</option>
+      </select>
+    </div>
+  </form>
   <CodeEditor
     autoFocus={true}
     content={state.src}
@@ -52,8 +88,21 @@ export default class HTMLPrettier extends React.Component<object, State> {
 
   private handleFormatClick = () => {
     const { state } = this;
+    const opt: HTMLBeautifyOptions = {};
+    const indentOpt = state.indentOpt;
+    if (indentOpt === IndentOpt.tab) {
+      opt.indent_with_tabs = true;
+    } else if (indentOpt === IndentOpt.twoSpaces) {
+      opt.indent_with_tabs = false;
+      opt.indent_size = 2;
+    } else {
+      opt.indent_with_tabs = false;
+      opt.indent_size = 4;
+    }
+
     this.setState({
-      dest: prettier.html_beautify(state.src || ''),
+      // tslint:disable-next-line no-any
+      dest: html_beautify(state.src || '', opt as any),
     }, () => {
       this.selectCodeEditor();
     });
