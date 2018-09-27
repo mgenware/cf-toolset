71653dcf7f170edcb2f8044df4c6615ad9234301
Mgen <m@mgenware.com>
2018-09-27 16:21:34 +0800

    Clean unused components

--- src/router.ts
+++ src/router.ts
@@ -1,33 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: 'toolset',
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
     {
       path: '/url-encoder-decoder',
       name: 'url-encoder-decoder',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './toolset/URLDataEncoderDecoder.vue'),
+      component: () => import(/* webpackChunkName: "URLDataEncoderDecoder" */ './toolset/URLDataEncoderDecoder.vue'),
     },
   ],
 });
