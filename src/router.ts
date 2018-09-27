c6694ace8fca1265634105c1f78c8cf5df22f8f9
Mgen <m@mgenware.com>
2018-09-27 20:10:02 +0800

    Use webpack dynamic imports

--- src/router.ts
+++ src/router.ts
@@ -1,31 +1,20 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import rawRoutes from './routes.json';
 
 Vue.use(Router);
+
+const routes = rawRoutes.routes.map((r: any) => {
+  const [fileName, urlName] = r;
+  return {
+    name: urlName,
+    path: `/${urlName}`,
+    component: () => import(/* webpackChunkName: "[request]" */ `./toolset/${fileName}.vue`),
+  };
+});
 
 export default new Router({
   mode: 'history',
   base: 'toolset',
-  routes: [
-    {
-      path: '/url-data-encoder-decoder',
-      name: 'url-data-encoder-decoder',
-      component: () => import(/* webpackChunkName: "URLDataEncoderDecoder" */ './toolset/URLDataEncoderDecoder.vue'),
-    },
-    {
-      path: '/url-unicode-transformer',
-      name: 'url-unicode-transformer',
-      component: () => import(/* webpackChunkName: "URLUnicodeTransformer" */ './toolset/URLUnicodeTransformer.vue'),
-    },
-    {
-      path: '/html-data-encoder-decoder',
-      name: 'html-data-encoder-decoder',
-      component: () => import(/* webpackChunkName: "HTMLDataEncoderDecoder" */ './toolset/HTMLDataEncoderDecoder.vue'),
-    },
-    {
-      path: '/xml-data-encoder-decoder',
-      name: 'xml-data-encoder-decoder',
-      component: () => import(/* webpackChunkName: "XMLDataEncoderDecoder" */ './toolset/XMLDataEncoderDecoder.vue'),
-    },
-  ],
+  routes,
 });
