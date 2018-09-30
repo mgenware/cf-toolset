2ebb519d9bc8aafa75277055bd04f7114588e7a2
Mgen <m@mgenware.com>
2018-10-01 01:56:59 +0800

    Setup bulma routines

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
 import './ls/inject_en';
 import 'bulma/css/bulma.css';
 import './core_entry';
+import bulmaSetup from './bulmaSetup';
+
+function ready(fn: () => void) {
+  const doc = document as any;
+  if (doc.attachEvent ? doc.readyState === 'complete' : doc.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(() => {
+  try {
+    bulmaSetup();
+  } catch (ex) {
+    // tslint:disable-next-line no-console
+    console.log(`Bulma setup failed, ${ex.message}`);
+  }
+});
