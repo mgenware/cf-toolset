9b64b3c340f71589fa92dd089411a1f6374d9ade
Mgen <m@mgenware.com>
2018-10-17 20:51:58 +0800

    Fix prod_entry not found

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ if (!isProd) {
 module.exports = {
   baseUrl: isProd ? '/static/toolset/' : '/',
   pages: {
-    main: 'src/prod_entry.ts',
+    main: 'src/entry_prod.ts',
     ls_cs: 'src/ls/inject_cs.js',
     ls_en: 'src/ls/inject_en.js',
   },
